167e3deea41508b35d66917c4c4c440811feeb80
jconway4 <jconwa4@illinois.edu>
2018-03-14 19:02:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ var updatePlaylist = function(playlist, artistName, songTitle) {
 };
 
 var removeFromPlaylist = function(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 };
